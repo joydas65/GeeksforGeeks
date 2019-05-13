cddfd46692ee339af0a7a96a59a61d3f715f8031
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-05-13 23:44:26 +0530

    Create Count_ways_to_Nth_Stair_Order_doesnt_matter.java

---
+++ Count_ways_to_Nth_Stair_Order_doesnt_matter.java
@@ -0,0 +1,18 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0){
+		    int n = sc.nextInt();
+		    System.out.println((n/2)+1);
+		    t--;
+		}
+	}
+}
